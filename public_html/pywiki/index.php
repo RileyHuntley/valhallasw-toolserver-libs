806642bf4cf88cb805792ac97f68ca96c45426f1
valhallasw <valhallasw@3df0c289-831d-0410-969d-dce3b8f6be54>
2007-07-10 18:42:08 +0000

    Updated
    git-svn-id: svn://localhost:60332@16 3df0c289-831d-0410-969d-dce3b8f6be54

---
+++ public_html/pywiki/index.php
@@ -0,0 +1,48 @@
+<html><head><title>PyWikipediaBot Nightlies</title><link rel="stylesheet" href="/~valhallasw/main.css" /></head>
+<body>
+<h1>PyWikipediaBot Nightlies</h1>
+<p><h2>Nightly downloads</h2>
+The nightlies are generated every night at 20:00 UTC. You can view:
+<ul>
+<li>The <a href="/~valhallasw/pywiki/update-log/">cvs update</a> logs</li>
+<li>The packaging logs:
+  <ul>
+  <li><a href="/~valhallasw/pywiki/log-tar-bzip2.log">bzip2</a></li>
+  <li><a href="/~valhallasw/pywiki/log-tar-gzip.log">gzip</a></li>
+  <li><a href="/~valhallasw/pywiki/log-zip.log">zip</a></li>
+  </ul>
+</li>
+<li>The packages:
+  <ul>
+  <li><a href="/~valhallasw/pywiki/pywikipedia-nightly.tar.bz2">bzip2'ed tar</a> format</li>
+  <li><a href="/~valhallasw/pywiki/pywikipedia-nightly.tar.gz">gzip'ed tar</a> format</li>
+  <li><a href="/~valhallasw/pywiki/pywikipedia-nightly.zip">pkzip</a> format</li>
+  </ul>
+</li>
+</ul></p>
+<p><h2>FAQ</h2>
+<ul>
+<li>
+Q: What are those nasty CVS/ directories for?<br/>
+A: I am too lazy to no pack them and it's useable for people too lazy to do a normal checkout. This allows people to use - for example - <a href="http://www.tortoisecvs.org">TortoiseCVS</a> without filling any scary forms. And it only takes 10 kB anyway.
+</li>
+<li>
+Q: How can I use the framework?<br/>
+A: Read the <a href="http://meta.wikimedia.org/wiki/Using_the_python_wikipediabot">manual</a>
+</li>
+<li>
+Q: OMG BUG!!!!!!1111oneoneone<br/>
+A: Report them at the <a href="http://sourceforge.net/tracker/?atid=603138&group_id=93107&func=browse">bug tracker</a>
+</li>
+<li>
+Q: I've got a support or feature request, or a patch<br/>
+A: <a href="http://sourceforge.net/tracker/?atid=603139&group_id=93107&func=browse">Support requests</a>, <a href="http://sourceforge.net/tracker/?atid=603141&group_id=93107&func=browse">Feature requests</a>, <a href="http://sourceforge.net/tracker/?atid=603140&group_id=93107&func=browse">Oatches</a>. Or join us on irc: <a href="irc://irc.freenode.net/pywikipediabot">#pywikipediabot on freenode</a>.
+</li>
+<li>
+Q: I want to stay updated<br/>
+A: Subscribe to the <a href="http://sourceforge.net/mail/?group_id=93107">mailing list</a>, join us on <a href="irc://irc.freenode.net/pywikipediabot">IRC</a>. </li>
+</ul>
+</p>
+<p class="footer">Latest pywikipedia nightly was generated at: <? include("latest") ?><br/>Web site revision: $Id$ </p>
+</body>
+</html>
